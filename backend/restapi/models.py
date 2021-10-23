1b9a192916b989a94589039c96cd13bdbb479ea1
dreadarceus <dreadarceus@gmail.com>
2021-10-23 14:03:45 +0530

    model ideas

--- backend/restapi/models.py
+++ backend/restapi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Course(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=40)
+    parts = models.IntegerField(default=1)
+
+
+class Student(models.Model):
+    mobile_num = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
+    courses = models.ManyToManyField(Course)
