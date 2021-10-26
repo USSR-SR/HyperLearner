ea14a0b107d5120767fad837b11c7cc33064a947
dreadarceus <dreadarceus@gmail.com>
2021-10-26 16:52:15 +0530

    createStudent fix2

--- backend/restapi/urls.py
+++ backend/restapi/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url('^getAllCourses$', getAllCourses),
     url('^createCourse',createCourse),
     url('^createStudent', createStudent),
-    url('^login',login),
+    url('^login$',login),
     url('^addFlash',addFlash),
     url('^getAllStudent', getAllStudent),
     url('^LinkCourse', AddSubjectToStudent),
