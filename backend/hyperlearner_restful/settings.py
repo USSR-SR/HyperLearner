b7aaeb60d9e7c3e8e7a6f20364c5347d7c0dff2d
dreadarceus <dreadarceus@gmail.com>
2021-10-26 10:29:31 +0530

    fix 0

--- backend/hyperlearner_restful/settings.py
+++ backend/hyperlearner_restful/settings.py
@@ -129,9 +129,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000/',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000/',
+# ]
 
 django_heroku.settings(locals())
