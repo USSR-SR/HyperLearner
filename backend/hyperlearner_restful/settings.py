41ab053b536cba6c86cfc41e27db5f966b1484ac
shivral somani <shivral312002@gmail.com>
2021-10-26 16:34:34 +0530

    lol

--- backend/hyperlearner_restful/settings.py
+++ backend/hyperlearner_restful/settings.py
@@ -133,7 +133,8 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000/',
 ]
-SESSION_COOKIE_SECURE= False
-SESSION_COOKIE_DOMAIN= 'http://localhost:3000/' 
+SESSION_ENGINE="django.contrib.sessions.backends.signed_cookie6s"
+# SESSION_COOKIE_SECURE= False
+# SESSION_COOKIE_DOMAIN= 'http://localhost:3000/' 
 
 django_heroku.settings(locals())
