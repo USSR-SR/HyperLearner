59225ce3910bedb6cd4bce90d68e359821f5cb97
shivral somani <shivral312002@gmail.com>
2021-10-26 16:28:38 +0530

    lol

--- backend/hyperlearner_restful/settings.py
+++ backend/hyperlearner_restful/settings.py
@@ -129,10 +129,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000/',
-# ]
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000/',
+]
 SESSION_COOKIE_SECURE= False
 SESSION_COOKIE_DOMAIN= 'http://localhost:3000/' 
 
