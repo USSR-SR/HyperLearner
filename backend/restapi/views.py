f1292026bcf399ed8e76f81ed5da21e93838a707
dreadarceus <dreadarceus@gmail.com>
2021-10-26 12:16:44 +0530

    createStudent fix2

--- backend/restapi/views.py
+++ backend/restapi/views.py
@@ -84,7 +84,7 @@ def login(request:HttpRequest):
 
             return JsonResponse({"message":"Logged in "},status=status.HTTP_200_OK)
         else:
-            return JsonResponse({"message":"Wrong Password "},status=status.HTTP_200_OK)
+            return JsonResponse({"message":"Wrong Password "},status=status.HTTP_204_NO_CONTENT)
     except:
         return JsonResponse({"message ":"not found user"},status=status.HTTP_400_BAD_REQUEST)
 
